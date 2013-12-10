8524c6f6dd3e5817e0f8a6498ef217d8b8404fbf
Reece Fowell <reece@codeconsortium.com>
2013-12-10 18:49:26 +0000

    WIP 2.0.x updates

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,22 +28,20 @@ class AppKernel extends Kernel
             new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
             new FOS\UserBundle\FOSUserBundle(),
 
-            // CCDN Main Bundles.
+            // CCDN Support Bundles.
             new CCDNComponent\CommonBundle\CCDNComponentCommonBundle(),
             new CCDNComponent\CrumbTrailBundle\CCDNComponentCrumbTrailBundle(),
             new CCDNComponent\BBCodeBundle\CCDNComponentBBCodeBundle(),
             new CCDNComponent\DashboardBundle\CCDNComponentDashboardBundle(),
             new CCDNComponent\AttachmentBundle\CCDNComponentAttachmentBundle(),
 
+			// CCDN Main Bundles.
             new CCDNUser\SecurityBundle\CCDNUserSecurityBundle(),
             new CCDNUser\UserBundle\CCDNUserUserBundle(),
             new CCDNUser\ProfileBundle\CCDNUserProfileBundle(),
-            new CCDNUser\MemberBundle\CCDNUserMemberBundle(),
             new CCDNUser\AdminBundle\CCDNUserAdminBundle(),
 
             new CCDNForum\ForumBundle\CCDNForumForumBundle(),
-            //new CCDNForum\AdminBundle\CCDNForumAdminBundle(),
-			
 			new CCDNMessage\MessageBundle\CCDNMessageMessageBundle(),
 			
 			// CCDN Child Bundles
@@ -76,4 +74,37 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+//	/**
+//	 * 
+//	 * @link http://kriswallsmith.net/post/27979797907/get-fast-an-easy-symfony2-phpunit-optimization
+//	 */
+//	protected function initializeContainer()
+//	{
+//        static $first = true;
+//
+//        if ('test' !== $this->getEnvironment()) {
+//            parent::initializeContainer();
+//            return;
+//        }
+//
+//        $debug = $this->debug;
+//
+//        if (!$first) {
+//            // disable debug mode on all but the first initialization
+//            $this->debug = false;
+//        }
+//
+//        // will not work with --process-isolation
+//        $first = false;
+//
+//        try {
+//            parent::initializeContainer();
+//        } catch (\Exception $e) {
+//            $this->debug = $debug;
+//            throw $e;
+//        }
+//
+//        $this->debug = $debug;
+//	}
 }

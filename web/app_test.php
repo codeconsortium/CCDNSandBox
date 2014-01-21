22ba083e3223b6e9cd68ae3af77ca05dde5b5b95
Reece Fowell <reece@codeconsortium.com>
2014-01-21 17:04:05 +0000

    changes for symfony 2.4

--- web/app_test.php
+++ web/app_test.php
@@ -7,10 +7,6 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-if (getenv('APP_ENV') !== 'test') {
-    exit;
-}
-
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
@@ -26,7 +22,7 @@ Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('test', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
